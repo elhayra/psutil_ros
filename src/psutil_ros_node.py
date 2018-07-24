95dfa4b5e284c6d55f7d2deec2d859f88a63b6cb
sub <elhayr1@gmail.com>
2018-07-24 21:47:02 +0300

    added network and memo messages

--- src/psutil_ros_node.py
+++ src/psutil_ros_node.py
@@ -7,6 +7,8 @@ from psutil_ros.msg import VirtualMemory
 from psutil_ros.msg import Network
 from psutil_ros.msg import Disk
 
+#TODO: CREATE LAUNCH FILE
+#TODO: check for psutil errors
 
 if __name__ == '__main__':
     
@@ -40,16 +42,24 @@ if __name__ == '__main__':
 	cpu_msg.usage = psutil.cpu_percent(interval=0)
 	cpu_msg.physical_cores = psutil.cpu_count(logical=False)
 	cpu_msg.cores = psutil.cpu_count()
+	cpu_msg.current_freq = psutil.cpu_freq().current
+	cpu_msg.min_freq = psutil.cpu_freq().min
+        cpu_msg.max_freq = psutil.cpu_freq().max
 
 	# read cpu data only after first cycle, because we are using interval=0, and psutil'''
 	# documentation says in this case, the first read should be ignored'''
 	if not is_first and publish_cpu: 
             cpu_pub.publish(cpu_msg)
             
-            
-            
         #**** Virtual Memo *****
         if publish_memo:
+            vm = psutil.virtual_memory()
+            memo_msg.total = vm.total
+            memo_msg.available = vm.available
+            memo_msg.used = vm.used
+            memo_msg.free = vm.free
+            memo_msg.percent = vm.percent
+            
             mem_pub.publish(memo_msg)
         
         #********* Disk ********  
@@ -58,6 +68,16 @@ if __name__ == '__main__':
             
         #******* Network *******   
         if publish_network:
+            net = psutil.net_io_counters()
+            network_msg.bytes_sent = net.bytes_sent
+            network_msg.bytes_recv = net.bytes_recv
+            network_msg.packets_sent = net.packets_sent
+            network_msg.packets_recv = net.packets_recv
+            network_msg.errin = net.errin
+            network_msg.errout = net.errout
+            network_msg.dropin = net.dropin
+            network_msg.dropout = net.dropout
+            
             network_pub.publish(network_msg)
         
         rate.sleep()
